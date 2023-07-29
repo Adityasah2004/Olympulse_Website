760e026ab1385390d7fc6f8fe1089023a2caf6f6
Adityasah2004 <sahaditya59@gmail.com>
2023-07-29 14:07:30 +0530

    [ADDED]margin

--- contender.js
+++ contender.js
@@ -6,6 +6,7 @@ import App from './frontend/app';
 
 
 
+
 const { createClient } = require('@supabase/supabase-js');
 
 const supabase = createClient('https://qcmzcfublrqyfgdiforw.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFjbXpjZnVibHJxeWZnZGlmb3J3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTAyMjA4NjUsImV4cCI6MjAwNTc5Njg2NX0.wTZbmgx2Z9_UZLnlUYy0kMyn2nnetIe4G06LPl5HX10');
